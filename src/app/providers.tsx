82bc47e01aea73d13c218e8ee8486cfaa42124e5
AgustinGarrone <agustinngarrone@hotmail.com>
2024-05-09 17:16:59 -0300

    fix: auth context

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,18 @@
-'use client'
-import { ChakraProvider } from "@chakra-ui/react"
-import { Layout } from "@/layout"
-import { QueryClient, QueryClientProvider } from "react-query"
-
-
+"use client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Layout } from "@/layout";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { AuthProvider } from "@/hooks/useAuth";
 
 export function Providers({ children }: { children: JSX.Element }) {
-
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <ChakraProvider >
-        <Layout>{children}</Layout>
-      </ChakraProvider>
-    </QueryClientProvider>
-  )
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider>
+          <Layout>{children}</Layout>
+        </ChakraProvider>
+      </QueryClientProvider>
+    </AuthProvider>
+  );
 }
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,8 +1,8 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { LoginPage } from "@/screen/login";
 import { useAuth } from "@/hooks/useAuth";
+import { LoginPage } from "@/screen/login";
 
 type Props = {
   children?: React.ReactNode;
@@ -11,20 +11,20 @@ type Props = {
 export const Layout: React.FC<Props> = ({ children }) => {
   const router = useRouter();
   const { isAuthenticated } = useAuth();
-  const [authState, setAuthState] = useState(false);
+  const [authState , setAuthState] = useState(false)
 
   useEffect(() => {
     const checkAuthAndRedirect = async () => {
       if (isAuthenticated()) {
-        setAuthState(true);
+        setAuthState(true)
       } else {
         router.push("/login");
-        setAuthState(false);
+        setAuthState(false)
       }
     };
 
     checkAuthAndRedirect();
   }, [isAuthenticated, router]);
 
-  return authState ? <div>{children}</div> : <LoginPage />;
+   return !authState ? <div>{children}</div> : <LoginPage />;
 };
